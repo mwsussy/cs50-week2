5bdd0c398df571ff4ecc44030b593c6c06e2885e
dhruvsaraswat_0 <163120267+mwsussy@users.noreply.github.com>
2025-05-05 18:48:29 +0530

    Create alphabetical.c

---
+++ alphabetical.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    // Get user's input
+    if (argc != 2)
+    {
+        printf("Please provide a word.\n");
+        return 1;
+    }
+
+    string text = argv[1];
+
+    for (int i = 0, len =strlen(text) ; i < len; i++)
+    {
+        if (!isalpha(text[i]))
+        {
+            printf("This is not a letter.\n");
+            return 2;
+        }
+    }
+
+    // Iterate through each element in the string
+    for (int i = 1; len = strlen(text); i < len; i++)
+    {
+        if (text [i] < text[i - 1])
+        {
+            printf("No\n");
+            return 0;
+        }
+    }
+      // Print out yes
+      printf("Yes\n");
+}
